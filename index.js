eb98082c06bfb2a5498f189ffe4b10aa003bdc11
Ryan Day <soldair@gmail.com>
2015-09-15 04:40:04 -0700

    adding async highlight code

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ var gravatar = require('./lib/gravatar')
 var headings = require('./lib/headings')
 var packagize = require('./lib/packagize')
 
-var marky = module.exports = function (markdown, options) {
+var i = 0
+
+var marky = module.exports = function (markdown, options, cb) {
   var html, $
 
   if (typeof markdown !== 'string') {
@@ -38,44 +40,60 @@ var marky = module.exports = function (markdown, options) {
   log('\n\n' + markdown + '\n\n')
 
   log('Parse markdown into HTML and add syntax highlighting')
+
+  // async
+  if (typeof cb === 'function') {
+    ++i
+    render(markdown, options, function (err, html, id) {
+      --i
+      if (err) return cb(err)
+      cb(false, _after(html || '', id))
+    })
+    return
+  }
+
+  // sync
   html = render(markdown, options)
+  return _after(html)
 
-  log('Convert HTML frontmatter into meta tags')
-  html = frontmatter(html)
+  function _after (html, id) {
+    log('Convert HTML frontmatter into meta tags')
+    html = frontmatter(html)
 
-  if (options.sanitize) {
-    log('Sanitize malicious or malformed HTML')
-    html = sanitize(html)
+    if (options.sanitize) {
+      log('Sanitize malicious or malformed HTML')
+      html = sanitize(html)
+    }
+
+    log('Parse HTML into a cheerio DOM object')
+    $ = cheerio.load(html)
+
+    log('Make gravatar image URLs secure')
+    $ = gravatar($)
+
+    log('Resolve relative GitHub link hrefs')
+    $ = github($, options.package)
+
+    log('Dress up Youtube iframes')
+    $ = youtube($)
+
+    log('Add CSS classes to paragraphs containing badges')
+    $ = badges($)
+
+    log('Add fragment hyperlinks links to h1,h2,h3,h4,h5,h6')
+    $ = headings($, options)
+
+    log('Apply CSS classes to readme content already expressed by package metadata')
+    $ = packagize($, options.package)
+
+    if (options.serveImagesWithCDN) {
+      log('Rewrite relative image source to use CDN')
+      $ = cdn($, options.package)
+    }
+
+    return $
   }
-
-  log('Parse HTML into a cheerio DOM object')
-  $ = cheerio.load(html)
-
-  log('Make gravatar image URLs secure')
-  $ = gravatar($)
-
-  log('Resolve relative GitHub link hrefs')
-  $ = github($, options.package)
-
-  log('Dress up Youtube iframes')
-  $ = youtube($)
-
-  log('Add CSS classes to paragraphs containing badges')
-  $ = badges($)
-
-  log('Add fragment hyperlinks links to h1,h2,h3,h4,h5,h6')
-  $ = headings($, options)
-
-  log('Apply CSS classes to readme content already expressed by package metadata')
-  $ = packagize($, options.package)
-
-  if (options.serveImagesWithCDN) {
-    log('Rewrite relative image source to use CDN')
-    $ = cdn($, options.package)
-  }
-
-  return $
-
 }
 
 marky.parsePackageDescription = packagize.parsePackageDescription
+marky.resetHighlighter = render.resetHighlighter
