88387c990b275844a9bf6f0ea4ee0119810d95a4
James Tautges <jamesdtautges@gmail.com>
2016-10-26 23:57:27 +0100

    Test parsing of raw html headers

---
+++ test/fixtures/htmlheading.md
@@ -0,0 +1,7 @@
+<h3>hello</h3>
+[a](a)
+[b](b)
+
+<h1>there</h1>
+[c](c)
+[d](d)
---
+++ test/htmlheading.js
@@ -0,0 +1,26 @@
+var assert = require('assert')
+var marky = require('..')
+var fixtures = require('./fixtures')
+var cheerio = require('cheerio')
+
+describe('html-headings', function () {
+    var $
+
+    before(function () {
+	$ = cheerio.load(marky(fixtures.htmlheading))
+    })
+
+    it('preserves the header tags', function () {
+	assert.equal($('h1').text(), 'there')
+	assert(~$('h2'))
+	assert.equal($('h3').text(), 'hello')
+    })
+
+    it('wraps the blocks in <p> tags', function () {
+	assert.equal($('p').length, 2)
+	assert.notEqual($('p a[href=a]').length, 0)
+	assert.notEqual($('p a[href=b]').length, 0)
+	assert.notEqual($('p a[href=c]').length, 0)
+	assert.notEqual($('p a[href=d]').length, 0)
+    })
+})
