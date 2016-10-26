2f50a6015673683c83ab90ec8eede75801ead733
James Tautges <jamesdtautges@gmail.com>
2016-10-27 00:00:15 +0100

    Enforce standard style on new unit test

--- test/htmlheading.js
+++ test/htmlheading.js
@@ -4,23 +4,23 @@ var fixtures = require('./fixtures')
 var cheerio = require('cheerio')
 
 describe('html-headings', function () {
-    var $
+  var $
 
-    before(function () {
-	$ = cheerio.load(marky(fixtures.htmlheading))
-    })
+  before(function () {
+    $ = cheerio.load(marky(fixtures.htmlheading))
+  })
 
-    it('preserves the header tags', function () {
-	assert.equal($('h1').text(), 'there')
-	assert(~$('h2'))
-	assert.equal($('h3').text(), 'hello')
-    })
+  it('preserves the header tags', function () {
+    assert.equal($('h1').text(), 'there')
+    assert(~$('h2'))
+    assert.equal($('h3').text(), 'hello')
+  })
 
-    it('wraps the blocks in <p> tags', function () {
-	assert.equal($('p').length, 2)
-	assert.notEqual($('p a[href=a]').length, 0)
-	assert.notEqual($('p a[href=b]').length, 0)
-	assert.notEqual($('p a[href=c]').length, 0)
-	assert.notEqual($('p a[href=d]').length, 0)
-    })
+  it('wraps the blocks in <p> tags', function () {
+    assert.equal($('p').length, 2)
+    assert.notEqual($('p a[href=a]').length, 0)
+    assert.notEqual($('p a[href=b]').length, 0)
+    assert.notEqual($('p a[href=c]').length, 0)
+    assert.notEqual($('p a[href=d]').length, 0)
+  })
 })
