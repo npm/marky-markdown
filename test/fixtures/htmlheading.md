43662280a77d2826b39b644c43d4dd80d53ccc64
James Tautges <jamesdtautges@gmail.com>
2016-11-04 23:11:18 +0000

    Test spacing before raw html header

--- test/fixtures/htmlheading.md
+++ test/fixtures/htmlheading.md
@@ -1,7 +1,19 @@
-<h3>hello</h3>
+<h1>hello</h1>
 [a](a)
 [b](b)
 
 <h1>there</h1>
 [c](c)
 [d](d)
+
+ <h1>one space</h1>
+one space
+
+  <h1>two spaces</h1>
+two spaces
+
+   <h1>three spaces</h1>
+three spaces
+
+    <h3>four spaces</h3>
+four spaces
--- test/htmlheading.js
+++ test/htmlheading.js
@@ -8,21 +8,34 @@ var cheerio = require('cheerio')
 describe('html-headings', function () {
   var $
 
-  before(function () {
-    $ = cheerio.load(marky(fixtures.htmlheading))
-  })
+  describe('structure', function () {
+    before(function () {
+      $ = cheerio.load(marky(fixtures.htmlheading))
+    })
 
-  it('preserves the header tags', function () {
-    assert.equal($('h1').text(), 'there')
-    assert(~$('h2'))
-    assert.equal($('h3').text(), 'hello')
-  })
+    it('preserves the header tags', function () {
+	var headers = $('h1')
+	assert(headers.length > 4)
+	var values = ['hello', 'there', 'one space', 'two spaces', 'three spaces']
+	for (var i = 0; i < values.length; i++) {
+	    assert.equal(headers[i].children[1].data, values[i])
+	}
+    })
 
-  it('wraps the blocks in <p> tags', function () {
-    assert.equal($('p').length, 2)
-    assert.notEqual($('p a[href=a]').length, 0)
-    assert.notEqual($('p a[href=b]').length, 0)
-    assert.notEqual($('p a[href=c]').length, 0)
-    assert.notEqual($('p a[href=d]').length, 0)
+    it('wraps the blocks in <p> tags', function () {
+      assert.notEqual($('p a[href=a]').length, 0)
+      assert.notEqual($('p a[href=b]').length, 0)
+      assert.notEqual($('p a[href=c]').length, 0)
+      assert.notEqual($('p a[href=d]').length, 0)
+    })
+
+      it('ignores up to three spaces', function () {
+	  assert($('h1').length == 5)
+      })
+
+      it('treats four spaces as code block', function () {
+	  assert.equal($('h3').length, 0)
+	  assert.equal($('pre > code').length, 1)
+      })
   })
 })
