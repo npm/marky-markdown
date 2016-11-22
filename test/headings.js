9861933797b66c6596b233776d7eb7fc5d916a69
Revin Guillen <rg@sevenite.com>
2016-11-22 15:24:06 -0800

    Zip together markdown and HTML heading tests
    Fixes #301

--- test/headings.js
+++ test/headings.js
@@ -6,55 +6,113 @@ var fixtures = require('./fixtures')
 var cheerio = require('cheerio')
 
 describe('headings', function () {
-  var $
+  var $, $md, $html
 
   before(function () {
-    $ = cheerio.load(marky(fixtures.dirty))
+    $md = cheerio.load(marky(fixtures.dirty))
+    $html = cheerio.load(marky(fixtures.htmlheading))
+  })
+
+  describe('HTML heading parsing', function () {
+    it('preserves HTML header tags', function () {
+      assert.equal($html('h1').length, 5)
+    })
+
+    it('wraps the blocks in <p> tags', function () {
+      assert.notEqual($html('p a[href=a]').length, 0)
+      assert.notEqual($html('p a[href=b]').length, 0)
+      assert.notEqual($html('p a[href=c]').length, 0)
+      assert.notEqual($html('p a[href=d]').length, 0)
+    })
+
+    it('ignores up to three spaces', function () {
+      assert($html('h1').length === 5)
+    })
+
+    it('treats four spaces as code block', function () {
+      assert.equal($html('h3').length, 0)
+      assert.equal($html('pre > code').length, 1)
+    })
   })
 
   it('does not parse indented ATX headings as headings', function () {
     assert(~fixtures.dirty.indexOf(' # indented h1'))
     assert(~fixtures.dirty.indexOf('  ## indented h2'))
     assert(~fixtures.dirty.indexOf('   ### indented h3'))
-    assert.equal($('#indented h1').length, 0)
-    assert.equal($('#indented h2').length, 0)
-    assert.equal($('#indented h3').length, 0)
-    assert.equal($('#indented p').length, 3)
+    assert.equal($md('#indented h1').length, 0)
+    assert.equal($md('#indented h2').length, 0)
+    assert.equal($md('#indented h3').length, 0)
+    assert.equal($md('#indented p').length, 3)
   })
 
   it('injects hashy anchor tags into headings that have DOM ids', function () {
     assert(~fixtures.dirty.indexOf('# h1'))
-    assert($("h1 a[href='#h1']").length)
+    assert($md("h1 a[href='#h1']").length)
+  })
+
+  it('injects hashy anchor tags into headings that have DOM ids (HTML)', function () {
+    assert(~fixtures.htmlheading.indexOf('<h1>one space</h1>'))
+    assert($html("h1 a[href='#one-space']").length)
   })
 
   it('adds anchor class to added heading links', function () {
     assert(~fixtures.dirty.indexOf('# h1'))
-    assert($("h1 a.anchor[href='#h1']").length)
+    assert($md("h1 a.anchor[href='#h1']").length)
+  })
+
+  it('adds anchor class to added heading links (HTML)', function () {
+    assert(~fixtures.htmlheading.indexOf('<h1>one space</h1>'))
+    assert($html("h1 a.anchor[href='#one-space']").length)
   })
 
   it('adds aria-hidden attr to added heading links', function () {
     assert(~fixtures.dirty.indexOf('# h1'))
-    assert($("h1 a.anchor[aria-hidden='true']").length)
+    assert($md("h1 a.anchor[aria-hidden='true']").length)
+  })
+
+  it('adds aria-hidden=true to added heading links (HTML)', function () {
+    assert(~fixtures.htmlheading.indexOf('<h1>one space</h1>'))
+    assert($html("h1 a.anchor[aria-hidden='true']").length)
   })
 
   it("doesn't inject links into headings that already contain markdown links", function () {
     assert(~fixtures.dirty.indexOf('### [h3](/already/linky)'))
-    assert($("h3 a[href='/already/linky']").length)
+    assert($md("h3 a[href='/already/linky']").length)
+  })
+
+  it("doesn't inject links into headings that already contain markdown links (HTML)", function () {
+    assert(~fixtures.htmlheading.indexOf('[hello](/already/linky)'))
+    assert($html("h1 a[href='/already/linky']").length)
   })
 
   it("doesn't inject links into headings that already contain inline HTML links", function () {
     assert(~fixtures.dirty.indexOf('### <a href="/already/inline/linky">h3</a>'))
-    assert($("h3 a[href='/already/inline/linky']").length)
+    assert($md("h3 a[href='/already/inline/linky']").length)
+  })
+
+  it("doesn't inject links into headings that already contain inline HTML links (HTML)", function () {
+    assert(~fixtures.htmlheading.indexOf('<a href="/already/inline/linky">there</a>'))
+    assert($html("h1 a[href='/already/inline/linky']").length)
   })
 
   it("doesn't inject links into headings that contain internal inline HTML links", function () {
     assert(~fixtures.dirty.indexOf('### Heading with embedded <a href="/internal/inline/linky">link</a>'))
-    assert($("h3 a[href='/internal/inline/linky']").length)
+    assert($md("h3 a[href='/internal/inline/linky']").length)
+  })
+
+  it("doesn't inject links into headings that contain internal inline HTML links (HTML)", function () {
+    assert(~fixtures.htmlheading.indexOf('<a href="/internal/inline/linky">link</a>'))
+    assert($html("h1 a[href='/internal/inline/linky']").length)
   })
 
   it('applies a prefix to generated DOM ids by default', function () {
     assert(~fixtures.dirty.indexOf('## h2'))
-    assert.equal($('h2 a#user-content-h2').length, 1)
+    assert.equal($md('h2 a#user-content-h2').length, 1)
+  })
+
+  it('applies a prefix to generated DOM ids by default (HTML)', function () {
+    assert(~fixtures.htmlheading.indexOf('<h1>one space</h1>'))
+    assert.equal($html('h1 a#user-content-one-space').length, 1)
   })
 
   it('allows id prefixes to be disabled with prefixHeadingIds', function () {
@@ -63,12 +121,27 @@ describe('headings', function () {
     assert.equal($('h4 a#this-is-a-test').length, 1)
   })
 
+  it('allows id prefixes to be disabled with prefixHeadingIds (HTML)', function () {
+    assert(~fixtures.htmlheading.indexOf('<h1>one space</h1>'))
+    $ = cheerio.load(marky(fixtures.htmlheading, {prefixHeadingIds: false}))
+    assert.equal($('h1 a#one-space').length, 1)
+  })
+
   it('puts icons inside the generated heading links', function () {
-    assert(!!$('a.anchor svg').length)
+    assert(!!$md('a.anchor svg').length)
+  })
+
+  it('puts icons inside the generated heading links (HTML)', function () {
+    assert(!!$html('a.anchor svg').length)
   })
 
   it("allows generated links' icons to be disabled", function () {
     $ = cheerio.load(marky(fixtures.dirty, {enableHeadingLinkIcons: false}))
+    assert.equal($('a.anchor svg').length, 0)
+  })
+
+  it("allows generated links' icons to be disabled (HTML)", function () {
+    $ = cheerio.load(marky(fixtures.htmlheading, {enableHeadingLinkIcons: false}))
     assert.equal($('a.anchor svg').length, 0)
   })
 
