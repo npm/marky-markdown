a56c8b24abe4f489717a06b8b52304b69fd1670f
Revin Guillen <rg@sevenite.com>
2016-11-22 17:03:40 -0800

    Replace github paths in `html_inline` links/images
    Fixes #304

--- test/repo-github.js
+++ test/repo-github.js
@@ -24,9 +24,21 @@ describe('when package repo is on github', function () {
     assert($("a[href='https://github.com/mark/wahlberg/blob/master/relative/file.js']").length)
   })
 
+  it('rewrites relative link hrefs to absolute (HTML)', function () {
+    assert(~fixtures.github.indexOf('<a href="html-page.html">'))
+    assert(~fixtures.github.indexOf('<a href="html-page-and-image.html">'))
+    assert($("a[href='https://github.com/mark/wahlberg/blob/master/html-page.html']").length)
+    assert($("a[href='https://github.com/mark/wahlberg/blob/master/html-page-and-image.html']").length)
+  })
+
   it('rewrites slashy relative links hrefs to absolute', function () {
     assert(~fixtures.github.indexOf('(/slashy/poo)'))
     assert($("a[href='https://github.com/mark/wahlberg/blob/master/slashy/poo']").length)
+  })
+
+  it('rewrites slashy relative links hrefs to absolute (HTML)', function () {
+    assert(~fixtures.github.indexOf('<a href="nested/link/image">'))
+    assert($("a[href='https://github.com/mark/wahlberg/blob/master/nested/link/image']").length)
   })
 
   it('leaves protocol-relative URLs alone', function () {
@@ -39,14 +51,26 @@ describe('when package repo is on github', function () {
     assert($("a[href='#header']").length)
   })
 
-  it('replaces relative img URLs with npm CDN URLs', function () {
+  it('replaces relative img URLs with github URLs', function () {
     assert(~fixtures.github.indexOf('![](relative.png)'))
     assert($("img[src='https://raw.githubusercontent.com/mark/wahlberg/master/relative.png']").length)
   })
 
-  it('replaces slashy relative img URLs with npm CDN URLs', function () {
+  it('replaces relative img URLs with github URLs (HTML)', function () {
+    assert(~fixtures.github.indexOf('<img src="html-image.png">'))
+    assert(~fixtures.github.indexOf('<img src="html-page-and-image.png" alt="alt text">'))
+    assert($("img[src='https://raw.githubusercontent.com/mark/wahlberg/master/html-image.png']").length)
+    assert($("img[src='https://raw.githubusercontent.com/mark/wahlberg/master/html-page-and-image.png']").length)
+  })
+
+  it('replaces slashy relative img URLs with github URLs', function () {
     assert(~fixtures.github.indexOf('![](/slashy/deep.png)'))
     assert($("img[src='https://raw.githubusercontent.com/mark/wahlberg/master/slashy/deep.png']").length)
+  })
+
+  it('replaces slashy relative img URLs with github URLs (HTML)', function () {
+    assert(~fixtures.github.indexOf('<img src="nested/link/image/image.png">'))
+    assert($("img[src='https://raw.githubusercontent.com/mark/wahlberg/master/nested/link/image/image.png']").length)
   })
 
   it('leaves protocol relative URLs alone', function () {
